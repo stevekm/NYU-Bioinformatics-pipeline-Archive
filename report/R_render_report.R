5759b2b89a8f890c12e573ada6ce0b92dc11e23a
SteveK <steve.at.nyu1@gmail.com>
2015-10-14 18:01:32 -0400

    Update report scripts

--- report/R_render_report.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/Rscript
-
-# intercept the arguments to the script
-args <- commandArgs(trailingOnly=TRUE) 
-
-# R -e "rmarkdown::render('knitr_example.Rmd’)"
-print(args)
-rmarkdown::render(args)
