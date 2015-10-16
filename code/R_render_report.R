2b778b516d8e65b6c60d5ad1c47ade093096e902
SteveK <steve.at.nyu1@gmail.com>
2015-10-16 14:29:03 -0400

    update report

--- code/R_render_report.R
+++ code/R_render_report.R
@@ -1,8 +1,16 @@
 #!/usr/bin/Rscript
+# this script is called by build_report.sh
+# this script takes an input argument in the form of a .Rmd file and 
+# compiles it based on the file's YAML header
 
 # intercept the arguments to the script
 args <- commandArgs(trailingOnly=TRUE) 
 
-# R -e "rmarkdown::render('knitr_example.Rmd’)"
-print(args)
+print(args) # probably do not need this; outputs the input file name
+
+# this command creates the output file
 rmarkdown::render(args)
+
+
+# this is an old version of the command, use this as a standalone on terminal outside of R
+# R -e "rmarkdown::render('knitr_example.Rmd’)"
--- code/build_report.sh
+++ code/build_report.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# this script will generate an RMarkdown report based on the analysis from the pipeline
+# currently pulling information from the ChIP-Seq pipeline
+# this can be adjusted in future version
+# run this script from the analysis directory; 
+# # ~/projects/Collaborator_lab-ChIPseq-2015-10-09$ ./code/build_report.sh
+
+
+# # # # 
+# SETUP
+# # # # 
+
 
 # the location of the final report to output
 FINAL_REPORT="./report/report_output/final_report.Rmd"
@@ -13,27 +24,42 @@ else
 	cp ./report/report_building_blocks/first_page.Rmd $FINAL_REPORT
 fi
 
-# look for alignment results
+
+# # # # 
+# BUILD REPORT
+# # # # 
+
+
+# for debug purposes, force the inclusion of these pages:
+cat ./report/report_building_blocks/chunk_peak_calling.Rmd >> $FINAL_REPORT
+
+# # look for alignment results : DEPRICATED
 if [ -d ./demo_dirs/chipseq_standard/alignments ]
 then
         # if the alignment results are present, then concatenate them to the final report
         cat ./report_building_blocks/chunk_alignment.Rmd >> $FINAL_REPORT
 fi
 
-# look for analysis results, such as the pca results
+# # look for analysis results, such as the pca results : DEPRICATED
 if [ -d ./demo_dirs/chipseq_standard/pca ]
 then
 	# if the pca results are present, then concatenate the pca report chunk to the final report
 	cat ./report_building_blocks/chunk_pca.Rmd >> $FINAL_REPORT
 fi
 
-# look for other results, such as the call peaks data (doesn't exist!)
+# # look for other results, such as the call peaks data (doesn't exist!) : DEPRICATED
 if [ -d ./demo_dirs/chipseq_standard/call_peaks ]
 then
 	cat ./report_building_blocks/chunk_peak_calling.Rmd >> $FINAL_REPORT
 fi
 
+
+# # # # 
+# FINISH
+# # # # 
+
+# add the last page of the report
 cat ./report/report_building_blocks/last_page.Rmd >> $FINAL_REPORT
 
-# at the end, compile the report
+# compile the report
 ./code/R_render_report.R $FINAL_REPORT
