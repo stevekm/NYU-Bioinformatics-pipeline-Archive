7f49990aeed19a196976c22c3c81d42634b4c26a
SteveK <steve.at.nyu1@gmail.com>
2015-10-21 15:26:56 -0400

    update test wd script

--- code/scripts-check_wd.sh
+++ code/scripts-check_wd.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "Scipt location is ${0}" # full path to script including script name
-echo -e " Initial wd is $(pwd), \n this is where you called the script from"
-echo "Changing wd to...  ${0%/*}"
-cd ${0%/*} # sets wd as location of the script, following symlinks
-# cd "$(dirname "$0")" # same as above
-# otherwise, wd is the pwd that was already set when script was called
-echo "Now the wd is ... $(pwd)"
-echo "with two arg passes, the wd would be ${0%/*}"
+echo " $(basename ${0%/*/*} ) " # <<<---- THIS is the one I want # return just the project dir, two levels up from the script
+echo " ${0%/*/*} " # file path that script was called from, two dirs up
+cd ${0%/*/*}
+echo $(pwd)
