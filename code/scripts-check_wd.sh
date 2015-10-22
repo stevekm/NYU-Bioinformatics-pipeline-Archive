80ffd02ada31af3a4bf896681e1de0b1d76a9d6c
SteveK <steve.at.nyu1@gmail.com>
2015-10-22 16:03:43 -0400

    update test script

--- code/scripts-check_wd.sh
+++ code/scripts-check_wd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo " $(basename ${0%/*/*} ) " # <<<---- THIS is the one I want # return just the project dir, two levels up from the script
+echo " $(basename ${0%/*/*} ) " # return just the project dir, two levels up from the script
 echo " ${0%/*/*} " # file path that script was called from, two dirs up
 cd ${0%/*/*}
+
+echo "The pwd is: "
 echo $(pwd)
