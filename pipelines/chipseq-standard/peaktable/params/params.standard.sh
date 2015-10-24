5ccdadb20e9e9ed89f12c732870253fbb8090cff
Aristotelis Tsirigos <tsirigos@gmail.com>
2015-10-24 11:21:16 -0400

    added peaktable step in the pipeline

---
+++ pipelines/chipseq-standard/peaktable/params/params.standard.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+peakdist=500      # max distances between peaks that will be merged
+
+annot_params="-i --upstream-max 100000 --upstream-min 100000 --distance-flag inputs/release/gene.split.bed"
+
