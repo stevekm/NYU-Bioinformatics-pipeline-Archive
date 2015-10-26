778b83c2c3630fc203196c8eda59144e6aafb366
SteveK <steve.at.nyu1@gmail.com>
2015-10-26 19:10:27 -0400

    Update build report, add results copy script

--- code/scripts-check_wd.sh
+++ code/scripts-check_wd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "initial wd is :"
+echo "$0"
 echo " $(basename ${0%/*/*} ) " # return just the project dir, two levels up from the script
 echo " ${0%/*/*} " # file path that script was called from, two dirs up
 cd ${0%/*/*}
